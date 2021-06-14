a69cadcb7cc84f74743e5d53871c96bade32d378
udaybajare_bluescript <uday.bajare@bluescripts.in>
2021-06-14 17:02:26 +0530

    Removed not required imports

--- src/main/java/com/adsquare/api/Application.java
+++ src/main/java/com/adsquare/api/Application.java
@@ -1,8 +1,6 @@
 package com.adsquare.api;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
